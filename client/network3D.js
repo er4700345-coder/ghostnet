2c3b69379bb3c357644e476d3e9f10c31f01193d
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-03-13 14:07:11 +0100

    Update network3D.js

--- client/network3D.js
+++ client/network3D.js
@@ -4,24 +4,24 @@ export default class Network3D {
 
 constructor(){
 
-this.nodes = {}
+this.nodes={}
 
-this.scene = new THREE.Scene()
+this.scene=new THREE.Scene()
 
-this.camera = new THREE.PerspectiveCamera(
+this.camera=new THREE.PerspectiveCamera(
 75,
 window.innerWidth/window.innerHeight,
 0.1,
 1000
 )
 
-this.renderer = new THREE.WebGLRenderer()
+this.renderer=new THREE.WebGLRenderer()
 
-this.renderer.setSize(window.innerWidth,500)
+this.renderer.setSize(window.innerWidth,400)
 
-document.body.appendChild(this.renderer.domElement)
+document.getElementById("network3d").appendChild(this.renderer.domElement)
 
-this.camera.position.z = 30
+this.camera.position.z=30
 
 this.animate()
 
@@ -29,40 +29,44 @@ this.animate()
 
 addNode(id){
 
-const geometry = new THREE.SphereGeometry(0.5)
+const geometry=new THREE.SphereGeometry(0.5)
 
-const material = new THREE.MeshBasicMaterial({
+const material=new THREE.MeshBasicMaterial({
+
 color:0x00ff9f
+
 })
 
-const sphere = new THREE.Mesh(geometry,material)
+const sphere=new THREE.Mesh(geometry,material)
 
-sphere.position.x = Math.random()*20-10
-sphere.position.y = Math.random()*20-10
-sphere.position.z = Math.random()*20-10
+sphere.position.x=Math.random()*20-10
+sphere.position.y=Math.random()*20-10
+sphere.position.z=Math.random()*20-10
 
 this.scene.add(sphere)
 
-this.nodes[id] = sphere
+this.nodes[id]=sphere
 
 }
 
 connect(a,b){
 
-if(!this.nodes[a] || !this.nodes[b]) return
+if(!this.nodes[a]||!this.nodes[b])return
 
-const material = new THREE.LineBasicMaterial({
+const material=new THREE.LineBasicMaterial({
+
 color:0x5555ff
+
 })
 
-const points = []
+const points=[]
 
 points.push(this.nodes[a].position)
 points.push(this.nodes[b].position)
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points)
+const geometry=new THREE.BufferGeometry().setFromPoints(points)
 
-const line = new THREE.Line(geometry,material)
+const line=new THREE.Line(geometry,material)
 
 this.scene.add(line)
 
@@ -72,7 +76,7 @@ animate(){
 
 requestAnimationFrame(()=>this.animate())
 
-this.scene.rotation.y += 0.001
+this.scene.rotation.y+=0.001
 
 this.renderer.render(this.scene,this.camera)
 

7a112c0c0270c09e8e678a91eea59054bbcdb4dc
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-03-13 13:57:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,179 @@
+import WebRTCManager from "./webrtc.js"
+import DHT from "./dht.js"
+import Encryption from "./encryption.js"
+import OnionRouter from "./onionRouter.js"
+import FileShare from "./fileShare.js"
+import PeerReputation from "./peerReputation.js"
 import NetworkGraph from "./networkGraph.js"
+import DecentralizedDNS from "./decentralizedDNS.js"
+import RoutingEngine from "./routingEngine.js"
+import LatencyMonitor from "./latencyMonitor.js"
 
-const graph = new NetworkGraph()
+const socket = new WebSocket("ws://localhost:3000")
 
-graph.addNode(peerId)
+let peerId
+let webrtc
+let dht
+let encryption
+let onion
+let fileShare
+let reputation
+let graph
+let dns
+let router
+let latency
+
+function log(msg){
+
+    const consoleUI = document.getElementById("console")
+
+    if(consoleUI){
+
+        consoleUI.innerHTML += msg + "<br>"
+        consoleUI.scrollTop = consoleUI.scrollHeight
+
+    }
+
+}
+
+socket.onmessage = async (event)=>{
+
+    const data = JSON.parse(event.data)
+
+    if(data.type === "INIT"){
+
+        peerId = data.peerId
+
+        log("Peer ID: " + peerId)
+
+        webrtc = new WebRTCManager(socket, peerId)
+
+        dht = new DHT(webrtc)
+
+        encryption = new Encryption()
+
+        onion = new OnionRouter(webrtc)
+
+        fileShare = new FileShare(webrtc)
+
+        reputation = new PeerReputation()
+
+        graph = new NetworkGraph()
+
+        dns = new DecentralizedDNS(dht)
+
+        router = new RoutingEngine(webrtc, reputation)
+
+        latency = new LatencyMonitor(webrtc, router)
+
+        graph.addNode(peerId)
+
+        registerHandlers()
+
+        socket.send(JSON.stringify({
+
+            type:"DISCOVER"
+
+        }))
+
+    }
+
+    if(data.type === "PEER_LIST"){
+
+        for(const peer of data.peers){
+
+            webrtc.connectToPeer(peer)
+
+            graph.addNode(peer)
+
+            graph.connect(peerId, peer)
+
+            reputation.addPeer(peer)
+
+        }
+
+    }
+
+    if(data.type === "SIGNAL"){
+
+        await webrtc.handleSignal(data.from, data.signal)
+
+    }
+
+}
+
+function registerHandlers(){
+
+    webrtc.registerHandler("PING",(from,packet)=>{
+
+        latency.handlePing(from,packet)
+
+    })
+
+    webrtc.registerHandler("PONG",(from,packet)=>{
+
+        latency.handlePong(from,packet)
+
+    })
+
+    webrtc.registerHandler("DHT_STORE",(from,packet)=>{
+
+        dht.handleStore(packet.hash, packet.value)
+
+    })
+
+    webrtc.registerHandler("DHT_FIND",(from,packet)=>{
+
+        dht.handleFind(packet.hash, packet.requestId, from)
+
+    })
+
+    webrtc.registerHandler("DHT_RESPONSE",(from,packet)=>{
+
+        dht.handleResponse(packet.requestId, packet.value)
+
+    })
+
+    webrtc.registerHandler("FILE_CHUNK",(from,packet)=>{
+
+        log("Receiving file chunk from " + from)
+
+    })
+
+    webrtc.registerHandler("ONION_PACKET",(from,packet)=>{
+
+        onion.handlePacket(packet,from)
+
+    })
+
+}
+
+document.getElementById("store").onclick = ()=>{
+
+    dht.store("hello","GhostNet World")
+
+    log("Stored key in DHT")
+
+}
+
+document.getElementById("find").onclick = async ()=>{
+
+    const value = await dht.find("hello")
+
+    log("DHT result: " + value)
+
+}
+
+document.getElementById("file").onchange = (e)=>{
+
+    const file = e.target.files[0]
+
+    if(file){
+
+        fileShare.shareFile(file)
+
+        log("Sharing file: " + file.name)
+
+    }
+
+}
